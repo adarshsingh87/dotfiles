05a43a6560d1ac3918e04f5aa343e367c9fbd0d3
Adarsh Singh <adarshmanoj.05.as@gmail.com>
2025-11-14 14:41:05 +0530

    feat: supermaven in nvim + wallpapers + terminal configs

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,6 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 alias btop="btop --force-utf"
+alias cd="z"
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
@@ -9,4 +10,4 @@ alias btop="btop --force-utf"
 string match -q "$TERM_PROGRAM" kiro and . (kiro --locate-shell-integration-path fish)
 
 set -x TERM xterm-256color
-nvm use v20.12.2
+zoxide init fish | source
